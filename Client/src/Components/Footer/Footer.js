11aac62e00f48bfad1b954fcb12b2503b7f69835
Kaushal Prakash <147122977+kaushal-Prakash@users.noreply.github.com>
2025-09-19 12:27:12 +0530

    Update Footer.js

--- Client/src/Components/Footer/Footer.js
+++ Client/src/Components/Footer/Footer.js
@@ -11,11 +11,22 @@ const Footer = (props) => {
           <p>{props.title}</p>
         </Link>
       </div>
+      
+      <div className="team-section">
+        <h3>Our Team</h3>
+        <div className="team-members">
+          <p>Kaushal Prakash</p>
+          <p>Jayantilal</p>
+          <p>Ibaad</p>
+          <p>Manik Salaria</p>
+        </div>
+      </div>
+      
       <div className="below-footer">
         <p>
-          You can reach me at{" "}
-          <a className="mail-links" href="mailto:maniksalaria890@gmail.com">
-            maniksalaria890@gmail.com
+          You can reach us at{" "}
+          <a className="mail-links" href="mailto:savagegamer1289@gmail.com">
+            savagegamer1289@gmail.com
           </a>
         </p>
         <p>
@@ -35,6 +46,33 @@ const Footer = (props) => {
             rel="noopener noreferrer"
           >
             <i className="fa fa-github"></i> GitHub
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <a
+            className="contact-links"
+            href="https://www.instagram.com/manik_276/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-instagram"></i> Instagram
+          </a>
+          &nbsp;&nbsp;|&nbsp;&nbsp;
+          <a
+            className="contact-links"
+            href="https://api.whatsapp.com/qr/GXRHM7PEPNOKA1?autoload=1&app_absent=0"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa fa-whatsapp"></i> WhatsApp
+          </a>
+        </p>
+        <p>&copy; 2025 PawFinds Team - Kaushal Prakash, Jayantilal, Ibaad, Manik Salaria</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;            <i className="fa fa-github"></i> GitHub
           </a>
           &nbsp;&nbsp;|&nbsp;&nbsp;
           <a
