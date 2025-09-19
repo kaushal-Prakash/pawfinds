e9faa2499efcf176d9b5c699c059cd6d6628dfd2
Jaymali369 <167812882+Jayumali369@users.noreply.github.com>
2025-09-19 12:16:05 +0530

    Update Contact.js

--- Client/src/Components/Contact/Contact.js
+++ Client/src/Components/Contact/Contact.js
@@ -7,27 +7,27 @@ const Contact = () => {
       <div className="contactUs-left-para">
         <h3>Let's get in touch</h3>
         <i class="fa fa-envelope"></i>
-        <a class="mail-links" href="mailto:kashifkzmi5@gmail.com">
-          kashifkzmi5@gmail.com
+        <a class="mail-links" href="mailto:maniksalaria890@gmail.com">
+          maniksalaria890@gmail.com
         </a>
 
         <i class="fa fa-linkedin"></i>
-        <a class="mail-links" href="https://www.linkedin.com/in/kashiekzmi/">
-          User Name: kashiekzmi
+        <a class="mail-links" href="https://www.linkedin.com/in/manik-salaria-095501280/">
+          User Name: maniksalaria
         </a>
 
         <i class="fa fa-github"></i>
-        <a class="mail-links" href="https://github.com/KaShiekzmi">
-          KaShiekzmi
+        <a class="mail-links" href="https://github.com/kaushal-Prakash/pawfinds">
+          maniksalaria
         </a>
 
         <i class="fa fa-instagram"></i>
-        <a class="mail-links" href="https://www.instagram.com/kaxhie_x/">
-          @kaxhie_x
+        <a class="mail-links" href="https://www.instagram.com/manik_276/">
+          @manik_276
         </a>
 
         <i class="fa fa-phone"></i>
-        <a class="mail-links" href="tel:+923019583959">
+        <a class="mail-links" href="tel:+8899772696">
           +92 301 9583959
         </a>
       </div>
